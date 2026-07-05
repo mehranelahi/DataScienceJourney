fe036e06dcaa2567d37911867697ccfa27fd2beb
mehranelahi <mehranelahi.dev@gmail.com>
2026-07-05 22:27:21 +0330

    Add some logics

--- Python/day05.py
+++ Python/day05.py
@@ -18,6 +18,37 @@ for i in range(len(student_names)):
     print(f"{student_names[i]}: {scores[i]}")
 
 
+
+print("----- Statistics -----")
+
+# Average score
+average_score = sum(scores) / len(scores)
+print(f"Average Score: {average_score}")
+
+# Highest score
+highest_score = max(scores)
+print(f"Highest Score: {highest_score}")
+
+# Lowest score
+lowest_score = min(scores)
+print(f"Lowest Score: {lowest_score}")
+
+# Top student logic
 highest_index = scores.index(max(scores))
-print(f"The top student is: {student_names[highest_index]}")
+top_student = student_names[highest_index]
+print(f"Top Student: {top_student}")
+
+# Passed and Faild Students
+passed = 0
+faild = 0
+for score in scores:
+    if((score * 5)  >= 70):
+        passed += 1
+    else:
+        faild += 1
+print(f"Passed Students: {passed}")
+print(f"Faild Students: {faild}")
+
+
+
 
