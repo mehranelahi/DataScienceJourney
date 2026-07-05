7b58c358ec7dee99d30c54069e5701a79b0508db
mehranelahi <mehranelahi.dev@gmail.com>
2026-07-05 20:20:41 +0330

    for loop

---
+++ Python/day05.py
@@ -0,0 +1,21 @@
+# numbers = [10, 20, 30, 40, 50]
+
+# for number in numbers:
+#     print(number)
+
+
+# student_names = ["Ali","Sara","Reza"]
+
+# for student in student_names:
+#     print(student)
+
+student_names = ["Ali","Sara","Reza"]
+scores = [18,20,15]
+
+print("----- Student List -----")
+
+for i in range(len(student_names)):
+    print(f"{student_names[i]}: {scores[i]}")
+
+
+
