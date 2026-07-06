6b3ad17de437ceea74b0d91786ba78488b8760d1
mehranelahi <mehranelahi.dev@gmail.com>
2026-07-06 21:13:32 +0330

    Added longest name feature

--- Python/day06.py
+++ Python/day06.py
@@ -1,12 +1,22 @@
 file = open("students.txt", "r")
 
 count = 0 
+longest_name = ""
 
 for line in file:
     print(line.strip())
     count += 1
+    if len(line.strip()) > len(longest_name):
+        longest_name = line.strip()
+    
+    
+        
+    
+    
 print(f"\nTotal Students: {count}")
+print(f"Longest Name: {longest_name}")
+
+
 
 file.close()
-
 
