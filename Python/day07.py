596bd743eec1f38041c83334948c0ffc9556afb2
mehranelahi <mehranelahi.dev@gmail.com>
2026-07-07 13:59:15 +0330

    Add average Score feature

--- Python/day07.py
+++ Python/day07.py
@@ -1,12 +1,18 @@
 file = open("students.csv", "r")
+next(file) #Ignore first line (The header of the csv)
 
+total = 0
+count = 0
 for line in file:
     parts = line.strip().split(",")
     name = parts[0]
-    score = parts[1]
-    print(f"Student: {name}\n Score: {score}")
-    
+    score = int(parts[1])
+    print(f"Student: {name}\n Score: {score} \n")
+    total += score
+    count += 1
 
+average = total / count    
+print(f"Average Score: {average}")
 
 
 file.close()
