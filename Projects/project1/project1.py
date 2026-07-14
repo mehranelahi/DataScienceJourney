e75d19d7581b9855ee4b9751fef4eb82cd68db8d
mehranelahi <mehranelahi.dev@gmail.com>
2026-07-14 14:32:15 +0330

    search student's final code

--- Projects/project1/project1.py
+++ Projects/project1/project1.py
@@ -1,6 +1,4 @@
 
-
-
 print("\n==== Student Management System ====")
 options = ["Show Report","Search Student","Show Excellebt Students","Show Passed Students","Show Failed Students","Exit"]
 
@@ -44,24 +42,29 @@ def show_report():
     file.close()
     
 
-def search_student():
-    question = str(input("Enter student's name: ")) 
+def search_student(): 
     file = open("students.csv","r")
+    next(file)
     students = []
     scores = []
-    for line in file:
-        parts = line.strip().split()
-        student_name = parts[0]
-        student_score = parts[1]
-    students.append(student_name)
-    scores.append(student_score)
-    file.close()  
 
-    if question in students:
-        score = scores[students.index(question)]
-        print(score)
+    for line in file:
+        parts = line.strip().split(',')
+        name = parts[0]
+        score = int(parts[1])
+        students.append(name)
+        scores.append(score)
+
+    student_name = input("Enter student's name: ")
+
+    if student_name in students:
+        student_index = students.index(student_name)
+        student_score = scores[student_index]
+        print(f"{student_name}'s score is {student_score}")
     else:
         print("Student not found")
+    file.close()  
+
 
 
 
